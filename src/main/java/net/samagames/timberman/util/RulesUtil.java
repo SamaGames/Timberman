8f689d6309ef3d1fa2b66174dfec58bb8acc2f1f
frasse_l <frasse_l@epitech.eu>
2016-03-18 16:12:28 +0100

    Added rules book with SGAPI

--- src/main/java/net/samagames/timberman/util/RulesUtil.java
+++ src/main/java/net/samagames/timberman/util/RulesUtil.java
@@ -2,9 +2,8 @@ package net.samagames.timberman.util;
 
 import net.samagames.timberman.Timberman;
 
-import org.bukkit.Material;
+import net.samagames.tools.RulesBook;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
 
 public class RulesUtil
 {
@@ -17,40 +16,19 @@ public class RulesUtil
     {
         if (book != null)
             return book;
-
-        String[] raw = new String[]{
-                "\n   ]--------------[" +
-                "\n      " + Timberman.NAME_BICOLOR + "§0" +
-                "\n    par §lSamaGames§0" +
-                "\n   ]--------------[" +
-                "\n" +
-                "\n" +
-                "\n §11.§0 Objectif\n" +
-                "\n §12.§0 Comment jouer ?",
-
-                "\n    §lObjectif§0\n" +
-                "\n Vous devez couper" +
-                "\n un arbre sans vous" +
-                "\n prendre de branche.",
-
-                "\n §lComment jouer ?§0\n" +
-                " Utilisez la hache" +
-                "\n pour couper, et" +
-                "\n déplacez vous de" +
-                "\n droite à gauche" +
-                "\n pour esquiver.",
-
-                "\n\nJeu développé par :" +
-                "\n\n - §lRigner§0" +
-                "\n\nInspiré par le jeu de" +
-                "\n\n - §lJohnSHEPARD§0" +
-                "\n\n\n      SamaGames" + 
-                "\n Tout droits réservés."
-        };
-        ItemStack item = ItemsUtil.setItemMeta(Material.WRITTEN_BOOK, 1, (short)0, "&6&lLivre de règles", null);
-        BookMeta meta = (BookMeta)item.getItemMeta();
-        meta.addPage(raw);
-        item.setItemMeta(meta);
-        return item;
+        book = new RulesBook(Timberman.NAME_BICOLOR)
+                .addOwner("Rigner").addContributor("JonhSHEPARD")
+                .addPage("Objectifs",
+                        " Vous devez couper\n" +
+                        " un arbre sans vous\n" +
+                        " prendre de branche.")
+                .addPage("Comment jouer ?",
+                        " Utilisez la hache\n" +
+                        " pour couper, et\n" +
+                        " déplacez vous de\n" +
+                        " droite à gauche\n" +
+                        " pour esquiver.")
+                .toItemStack();
+        return book;
     }
 }
